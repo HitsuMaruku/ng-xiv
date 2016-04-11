9960ea8adad198aaffeeed54488be72a033fb9fa
hitsu <hitsu@HITSU-WIN>
2016-04-10 22:53:31 -0400

    Add project files.

---
+++ HMAngular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HMAngular.Startup))]
+namespace HMAngular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
